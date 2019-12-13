04faa044442d62c5517df4e4db36060a7590633d
Buddy Agyin <buddyagyin@Buddys-MacBook-Pro.local>
2019-12-12 17:14:03 -0700

    update menu , refractor less/js, add link to logo

--- javascript/team.js
+++ javascript/team.js
@@ -6,30 +6,18 @@ const menuOverlay = document.getElementById('menu-overlay');
 const menuItems = document.getElementById('menu-items');
 const main = document.getElementById('main');
 const heading = document.getElementById('heading');
+const menuContainer = document.querySelector('.menu-container');
 let timesClicked = 0;
 
 const onClick = () => {
     if(timesClicked < 1) {
         timesClicked++;
         hamburger.classList.toggle('is-active');
-        hamburger.style.position = 'fixed';
-        heading.style.paddingLeft = '50.25rem';
-        menuOverlay.style.left = "0";
-        menuOverlay.style.width = '20%';
-        menuOverlay.style.opacity = '0.9';
-        menuItems.style.display = 'flex';
-        menuOverlay.style.display = 'inherit';
-        menuOverlay.style.background = 'black';
+        menuContainer.style.display = 'flex';
     }else{
         timesClicked--;
         hamburger.classList.toggle('is-active');
-        hamburger.style.position = 'inherit';
-        heading.style.paddingLeft = '0';
-        menuOverlay.style.border = 'none';
-        menuOverlay.style.left = "80%";
-        menuOverlay.style.width = '0%';
-        menuOverlay.style.display = 'none';
-        menuItems.style.display = 'none';
+        menuContainer.style.display = 'none';
     }
 
 }
